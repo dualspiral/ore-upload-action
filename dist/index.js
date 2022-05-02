585717ac0e8fd903233b4419f196f4d84efd876f
Daniel Naylor <git@drnaylor.co.uk>
2022-05-02 18:51:58 +0100

    Add additional logging for axios errors

--- dist/index.js
+++ dist/index.js
@@ -15876,8 +15876,21 @@ const orePluginAction = (function() {
             return Promise.reject(`Deployment failed with error code ${response.status}.`)
           }
         })
-        .catch(rejection => {
-          core.setFailed(rejection);
+        .catch(error => {
+          if (error.response) {
+            // Request made and server responded
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+          } else if (error.request) {
+            // The request was made but no response was received
+            console.log(error.request);
+          } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log('Error', error.message);
+          }
+      
+          core.setFailed(error);
         })
       } catch (error) {
         core.setFailed(error.message);
