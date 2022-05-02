0f69de53237a647aec0f818a47442834803deabd
Daniel Naylor <git@drnaylor.co.uk>
2022-05-02 16:49:28 +0100

    Try to pick out the right file for uploading

--- index.js
+++ index.js
@@ -1,96 +1,140 @@
 const core = require('@actions/core');
 const artifact = require('@actions/artifact');
 const artifactClient = artifact.create();
-const fs = require("fs").promises;
+const fs = require("fs");
+const fsPromises = fs.promises;
 const FormData = require('form-data');
 const axios = require("axios").default
+const StreamZip = require('node-stream-zip');
+const path = require('path');
 
-function stripTrailingSlash(url) {
-  if (url.endsWith("/")) {
-    return url.substring(0, url.length - 1);
-  } else {
-    return url;
-  }
-}
+const orePluginAction = (function() {
 
-// most @actions toolkit packages have async methods
-async function run() {
-  try {
-    console.log("Starting deployment");
-    // Get the plugin
-    const pluginLocation = await artifactClient.downloadArtifact(core.getInput("plugin"));
-    const tag = core.getInput("tag");
-    const oreUrl = stripTrailingSlash(core.getInput("oreUrl"));
-    const projectId = core.getInput("projectId");
-    const apiKey = `OreApi apikey="${core.getInput("apiKey")}"`;
-
-    // Check to see if we have an artefact for the description, else consider it a string.
-    const descriptionString = core.getInput("description")
-    const descriptionInput =
-        await artifactClient.downloadArtifact(descriptionString)
-            .then(response => fs.readFile(response.downloadPath))
-            .then(buffer => buffer.toString())
-            // eslint-disable-next-line no-unused-vars
-            .catch(ignored => Promise.resolve(descriptionString))
-
-    const apiAuthUrl = `${oreUrl}/api/v2/authenticate`
-    const apiPostVersionUrl = `${oreUrl}/api/v2/${projectId}/versions`
-
-    // Start by authenticating with the Ore client
-    console.log("Attempting to authenticate against Ore v2");
-    const fileToSend = await fs.readFile(pluginLocation.downloadPath);
-    let infoToSend;
-    if (tag !== undefined && tag !== "") {
-      infoToSend = {
-        "create_forum_post": core.getBooleanInput("createForumPost"),
-        "description": descriptionInput,
-        "tags": core.getInput("tag")
-      }
+  function stripTrailingSlash(url) {
+    if (url.endsWith("/")) {
+      return url.substring(0, url.length - 1);
     } else {
-      infoToSend = {
-        "create_forum_post": core.getBooleanInput("createForumPost"),
-        "description": descriptionInput
+      return url;
+    }
+  }
+
+  function getJarFile(fileName) {
+    return fileName.match(/.jar$/) !== null
+  }
+
+  async function selectFile(directory, fileNamePredicate = fileName => true, fileContentsPredicate = (directoryName, fileName) => true) {
+    try {
+      const result = fs.readdirSync(directory).filter(fileName => {
+        if (fileNamePredicate(fileName)) {
+          return predicate(directory, fileName)
+        }
+      })[0];
+      return fsPromises.readFile(path.join(directory, result));
+    } catch (err) {
+      console.error(err);
+      core.setFailed(err.message)
+    }
+  }
+
+  async function checkForSpongePluginOrMcModInfoFile(directory, fileName) {
+    const zip = new StreamZip.async({ file: path.join(directory, fileName) });
+    try {
+      let isPlugin = zip.entryData('META-INF/sponge-plugins.json')
+        .then(x => Promise.resolve(true))
+        .catch(x => Promise.resolve(false));
+      if (!isPlugin) {
+        isPlugin = zip.entryData('mcmod.info')
+          .then(x => Promise.resolve(true))
+          .catch(x => Promise.resolve(false));
+      }
+      return isPlugin;
+    } finally {
+      await zip.close();
+    }
+  }
+
+  return {
+    // most @actions toolkit packages have async methods
+    run: async function() {
+      try {
+        console.log("Starting deployment");
+        // Get the plugin
+        const pluginLocation = await artifactClient.downloadArtifact(core.getInput("plugin"), options = { createArtifactFolder: true });
+        const tag = core.getInput("tag");
+        const oreUrl = stripTrailingSlash(core.getInput("oreUrl"));
+        const projectId = core.getInput("projectId");
+        const apiKey = `OreApi apikey="${core.getInput("apiKey")}"`;
+
+        // Check to see if we have an artefact for the description, else consider it a string.
+        const descriptionString = core.getInput("description")
+        const descriptionInput =
+            await artifactClient.downloadArtifact(descriptionString, options = { createArtifactFolder: true })
+                .then(response => selectFile(response.downloadPath, getJarFile, checkForSpongePluginOrMcModInfoFile))
+                .then(buffer => buffer.toString())
+                // eslint-disable-next-line no-unused-vars
+                .catch(ignored => Promise.resolve(descriptionString))
+
+        const apiAuthUrl = `${oreUrl}/api/v2/authenticate`
+        const apiPostVersionUrl = `${oreUrl}/api/v2/${projectId}/versions`
+
+        // Start by authenticating with the Ore client
+        console.log("Attempting to authenticate against Ore v2");
+
+        const fileToSend = await fsPromises.readFile(pluginLocation.downloadPath);
+        let infoToSend;
+        if (tag !== undefined && tag !== "") {
+          infoToSend = {
+            "create_forum_post": core.getBooleanInput("createForumPost"),
+            "description": descriptionInput,
+            "tags": core.getInput("tag")
+          }
+        } else {
+          infoToSend = {
+            "create_forum_post": core.getBooleanInput("createForumPost"),
+            "description": descriptionInput
+          }
+        }
+        axios.post(apiAuthUrl,
+            {
+              "expires_in": 120
+            },
+          {
+          headers: {
+            "Authorization": apiKey
+          }
+        }).then(response => {
+          if (response.status === 200) {
+            const sessionHeader = `OreKey session="${response.data.session}"`;
+            const formData = new FormData();
+            formData.append("plugin-info", infoToSend);
+            formData.append("plugin-file", fileToSend);
+            return axios.post(apiPostVersionUrl,
+                formData,
+                {
+                  headers: {
+                    "Authorization": sessionHeader
+                  }
+                });
+          } else {
+            return Promise.reject(`Did not complete authorisation (received status code ${response.status})`);
+          }
+        })
+        .then(response => {
+          if (response.status === 201) {
+            console.log("Deployment successful");
+            return Promise.resolve(response);
+          } else {
+            return Promise.reject(`Deployment failed with error code ${response.status}.`)
+          }
+        })
+        .catch(rejection => {
+          core.setFailed(rejection);
+        })
+      } catch (error) {
+        core.setFailed(error.message);
       }
     }
-    axios.post(apiAuthUrl,
-        {
-          "expires_in": 120
-        },
-      {
-      headers: {
-        "Authorization": apiKey
-      }
-    }).then(response => {
-      if (response.status === 200) {
-        const sessionHeader = `OreKey session="${response.data.session}"`;
-        const formData = new FormData();
-        formData.append("plugin-info", infoToSend);
-        formData.append("plugin-file", fileToSend);
-        return axios.post(apiPostVersionUrl,
-            formData,
-            {
-              headers: {
-                "Authorization": sessionHeader
-              }
-            });
-      } else {
-        return Promise.reject(`Did not complete authorisation (received status code ${response.status})`);
-      }
-    })
-    .then(response => {
-      if (response.status === 201) {
-        console.log("Deployment successful");
-        return Promise.resolve(response);
-      } else {
-        return Promise.reject(`Deployment failed with error code ${response.status}.`)
-      }
-    })
-    .catch(rejection => {
-      core.setFailed(rejection);
-    })
-  } catch (error) {
-    core.setFailed(error.message);
   }
-}
+});
 
-run();
+orePluginAction.run();
